8a28b7da628b88833756ba27babc7bf6ce5cfe18
Christoph Franke <hallo@christophfranke.info>
2023-12-08 03:32:01 +0200

    less frequent word translation

--- python/scheduler/main.py
+++ python/scheduler/main.py
@@ -12,7 +12,7 @@ print("Scheduler Running")
 
 # create_statistics()
 schedule.every(2).hours.do(statistics.jobs)
-schedule.every(5).seconds.do(dictionary.jobs)
+schedule.every(10).seconds.do(dictionary.jobs)
 schedule.every(60).seconds.do(articles.jobs)
 
 while True:
