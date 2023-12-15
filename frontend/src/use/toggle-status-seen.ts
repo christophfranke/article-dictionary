177b688a42f287662c69aff0bd80d978c70f95ed
Christoph Franke <hallo@christophfranke.info>
2023-12-15 13:49:49 +0200

    fix typescript

--- frontend/src/use/toggle-status-seen.ts
+++ frontend/src/use/toggle-status-seen.ts
@@ -5,10 +5,12 @@ export const useToggleStatusSeen = (dictionary: DictionaryCollection) => {
 		const shiftKey = event.shiftKey || event.metaKey || event.ctrlKey;
 	  const original = word.toLowerCase();
 	  const status = dictionary.find(original)?.status;
-	  if (['new', 'seen'].includes(status)) {
-			dictionary.updateWord(original, { status: shiftKey ? 'ignore' : 'known' });
-	  } else {
-	  	dictionary.updateWord(original, { status: 'seen' });
+	  if (status) {	  	
+		  if (['new', 'seen'].includes(status)) {
+				dictionary.updateWord(original, { status: shiftKey ? 'ignore' : 'known' });
+		  } else {
+		  	dictionary.updateWord(original, { status: 'seen' });
+		  }
 	  }
 	};
 
