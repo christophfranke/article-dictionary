eda1b7ac29d240793e2c86eceac0933eb81d5dbd
Christoph Franke <hallo@christophfranke.info>
2023-12-19 14:29:52 +0200

    update renewal script

--- nginx/renew-certificates.sh
+++ nginx/renew-certificates.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Configuration variables
-DOMAIN="articles.krito.de"
 EMAIL="public@krito.de"
 WEBROOT_PATH="/var/www/certbot"
 
 # Renewing the certificate
-certbot renew --webroot -w $WEBROOT_PATH -d $DOMAIN --email $EMAIL --agree-tos --no-eff-email
+certbot renew --webroot -w $WEBROOT_PATH --email $EMAIL --agree-tos --no-eff-email
 
 # Reload Nginx to apply the renewed certificate (optional, uncomment if needed)
 nginx -s reload
