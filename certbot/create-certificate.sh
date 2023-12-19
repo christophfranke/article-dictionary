336afbccecc505c97f8f600ddb1436c7880dc6ea
Christoph Franke <hallo@christophfranke.info>
2023-12-19 13:18:51 +0200

    fix certificate creation file

--- certbot/create-certificate.sh
+++ certbot/create-certificate.sh
@@ -6,5 +6,5 @@ EMAIL="public@krito.de"
 WEBROOT_PATH="/var/www/certbot"
 
 # Run Certbot
-docker-compose run --rm certbot certonly --webroot -w $WEBROOT_PATH -d $DOMAIN --email $EMAIL --agree-tos --no-eff-email
+docker compose run --rm certbot certonly --webroot -w $WEBROOT_PATH -d $DOMAIN --email $EMAIL --agree-tos --no-eff-email
 
