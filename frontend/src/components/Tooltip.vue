34e6a1b6ee105495cd91e8d5f633aae7b32eca5d
Christoph Franke <hallo@christophfranke.info>
2023-12-22 01:19:28 +0200

    1sec tooltip update instead of half second

--- frontend/src/components/Tooltip.vue
+++ frontend/src/components/Tooltip.vue
@@ -55,7 +55,7 @@ const markArticleAsSeen = (article: ArticleBase, index: number) => {
 
 
 // time how long a translation is shown
-const UPDATE_TIME = 500
+const UPDATE_TIME = 1000
 let original = ''
 let timeoutId: ReturnType<typeof setTimeout> | null = null
 watch(isVisible, (newValue, oldValue) => {
