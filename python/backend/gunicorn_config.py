aa74ace5895a9ce398ab7759a77de42921fbd558
Christoph Franke <hallo@christophfranke.info>
2023-12-27 23:19:42 +0200

    add timeout of 120s

--- python/backend/gunicorn_config.py
+++ python/backend/gunicorn_config.py
@@ -1,2 +1,3 @@
-workers = 1 # note that we need a session management system to handle multiple workers
+workers = 1  # note that we need a session management system to handle multiple workers
 bind = "0.0.0.0:5000"
+timeout = 120 
--- python/shared/text_processing/dictionary/add_content.py
+++ python/shared/text_processing/dictionary/add_content.py
@@ -8,8 +8,6 @@ def add_text(text, user_id, dictionary_collection, user_collection):
     add_words(words, user_id, dictionary_collection, user_collection)
 
 def add_words(words, user_id, dictionary_collection, user_collection):
-    new_words = []  # Array to collect yet-to-be-translated words
-
     source_language, target_language = get_languages(user_collection, user_id)
 
     for word in words:
@@ -23,31 +21,28 @@ def add_words(words, user_id, dictionary_collection, user_collection):
 
         if existing_word is None:
             # If the word is not in the dictionary, add it to the array
-            new_words.append(word)
+            new_word = {
+                'original': word,
+                'translations': [word],
+                'status': 'new',
+                'last_viewed': None,
+                'review_level': 0,
+                'needs_retranslate': True,
+                'needs_recount': True,
+                'needs_clustering': True,
+                'translation_origin': None,
+                'cluster_id': None,
+                'frequency': 1,
+                'source_language': source_language,
+                'target_language': target_language,
+                'user_id': ObjectId(user_id)
+            }
+
+            dictionary_collection.insert_one(new_word)
+            print(f"Word '{new_word['original']}' added to the dictionary.")
         else:
             dictionary_collection.update_one(
                 {'_id': existing_word['_id']},
                 {'$set': {'needs_recount': True}}
             )
 
-    # Iterate over the translations dictionary and add each word to the dictionary
-    for original in new_words:
-        new_word = {
-            'original': original,
-            'translations': [original],
-            'status': 'new',
-            'last_viewed': None,
-            'review_level': 0,
-            'needs_retranslate': True,
-            'needs_recount': True,
-            'needs_clustering': True,
-            'translation_origin': None,
-            'cluster_id': None,
-            'frequency': 1,
-            'source_language': source_language,
-            'target_language': target_language,
-            'user_id': ObjectId(user_id)
-        }
-
-        dictionary_collection.insert_one(new_word)
-        print(f"Word '{new_word['original']}' added to the dictionary.")
