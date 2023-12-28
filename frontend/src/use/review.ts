6ead4adee692fd68c6ea301c2a481e4cd628d02d
Christoph Franke <hallo@christophfranke.info>
2023-12-29 01:07:52 +0200

    refactored review logic to ts file

---
+++ frontend/src/use/review.ts
@@ -0,0 +1,105 @@
+import type { PartialWord } from '@/types';
+import type { DictionaryView } from '@/dictionary/view';
+
+
+export const calculateIdealReviewInterval = (word: PartialWord): number | null => {
+	if (word.reviewLevel === 0) {
+		return null;
+	}
+
+	if (word.reviewLevel === 1) {
+		return 0
+	}
+
+	// level 3 -> 3.5 days, doubles with each level
+	return 1.75 * Math.pow(2, word.reviewLevel - 2);
+};
+
+export const calculateNextDue = (word: PartialWord): Date | null => {
+	const idealReviewInterval = calculateIdealReviewInterval(word);
+	if (idealReviewInterval === null) {
+		return null;
+	}
+
+	const lastViewed = new Date(word.lastViewed);
+	return new Date(lastViewed.getTime() + idealReviewInterval * 1000 * 3600 * 24);
+}
+
+const calculateDue = (word: PartialWord): number => {
+  const now = new Date();
+  const lastViewed = new Date(word.lastViewed);
+  const timeSinceLastViewed = (now.getTime() - lastViewed.getTime()) / (1000 * 3600 * 24); // in days
+
+  if (word.reviewLevel === 0 || word.status === 'ignore') {
+    return 0;
+  }
+
+  if (word.reviewLevel === 1) {
+  	// for first review level, score is 1 if last viewed today, 0 if last viewed more than a day ago
+    return (1 - timeSinceLastViewed);
+  }
+
+  // Calculate ideal review time
+  // level 3 -> 3.5 days, doubles with each level
+  const idealReviewTime = 1.75 * Math.pow(2, word.reviewLevel - 2);
+
+  // Calculate score based on how close we are to ideal review time
+  return timeSinceLastViewed * (2 - timeSinceLastViewed) / idealReviewTime;
+};
+
+const calculateImportance = (word: PartialWord, maxFrequency: number): number => {
+	const frequency = word.frequency
+  if (frequency === 0) return 0;
+  if (maxFrequency === 1) return 1; // Edge case to handle if maxFreq is 1
+
+  // Logarithmic scale: importance goes from 1 (for freq 1) to 2 (for maxFreq)
+  return 1 + Math.log(frequency) / Math.log(maxFrequency);
+};
+
+
+export default (dictionary: DictionaryView) => {
+	const findWordForReview = (): string | null => {
+		const maxFreq = Math.max(...dictionary.all.value.map(word => word.frequency));
+
+	  let highestScore = 0; // do not select words with 0 score or less
+	  let wordToReview: any | null = null;
+
+	  for (const word of dictionary.items.value) {
+	  	const importance = calculateImportance(word, maxFreq);
+	  	const due = calculateDue(word);
+	  	const random = 0.2 * Math.random();
+	    const score = importance * due + random;
+	    if (score > highestScore) {
+	    	console.log(`Found new highest score for ${word.original}: ${score.toFixed(3)} (${importance.toFixed(2)}x${due.toFixed(2)} + ${random.toFixed(2)})`);
+	      highestScore = score;
+	      wordToReview = word;
+	    }
+	  }
+
+	  return wordToReview?.id || null
+	};
+
+	const pickSentence = (sentences: { text: string, words: string[] }[], word?: PartialWord): number => {
+		const scores = sentences.map(sentence => {
+			const words = sentence.words.map(word => dictionary.find(word));
+			const score = words.reduce((acc, w) => acc + (w?.id === word?.id ? 6 : w?.reviewLevel || 0), 0);
+			const random = 1 + Math.random();
+			return random * score / words.length;
+		});
+
+		// find index with maximum score
+		let maxIndex = 0;
+		for (let i = 1; i < scores.length; i++) {
+			if (scores[i] > scores[maxIndex]) {
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex;
+	};
+
+	return {
+		pickSentence,
+		findWordForReview,
+	}
+}
--- frontend/src/views/ReviewWordView.vue
+++ frontend/src/views/ReviewWordView.vue
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import type { WordDetail, PartialWord } from '@/types';
 import { useDictionaryView } from '@/use/dictionary';
+import useReview from '@/use/review';
 
 import ProcessedContent from '@/components/ProcessedContent.vue';
 import Tooltip from '@/components/Tooltip.vue';
@@ -32,83 +33,11 @@ const tooltipDisplay = {
   }	
 }
 
-const { dictionary, isLoading } = useDictionaryView();
 const recentlyShown: string[] = [];
-const findWordForReview = (): string | null => {
-  const now = new Date();
-
-	const maxFreq = Math.max(...dictionary.items.value.map(word => word.frequency));
-	const calculateImportance = (word: PartialWord): number => {
-		const frequency = word.frequency
-	  if (frequency === 0) return 0;
-	  if (maxFreq === 1) return 1; // Edge case to handle if maxFreq is 1
-
-	  // Logarithmic scale: importance goes from 1 (for freq 1) to 2 (for maxFreq)
-	  return 1 + Math.log(frequency) / Math.log(maxFreq);
-	};
-
-  const calculateScore = (word: PartialWord): number => {
-    const lastViewed = new Date(word.lastViewed);
-    const timeSinceLastViewed = (now.getTime() - lastViewed.getTime()) / (1000 * 3600 * 24); // in days
-
-    if (word.reviewLevel === 0 || word.status === 'ignore') {
-      return 0;
-    }
-
-    if (recentlyShown.includes(word.id)) {
-    	return 0;
-    }
-
-    if (word.reviewLevel === 1) {
-    	// for first review level, score is 1 if last viewed today, 0 if last viewed more than a day ago
-      return (1 - timeSinceLastViewed);
-    }
-
-    // Calculate ideal review time
-    // level 3 -> 3.5 days, doubles with each level
-    const idealReviewTime = 1.75 * Math.pow(2, word.reviewLevel - 2);
-
-    // Calculate score based on how close we are to ideal review time
-    return timeSinceLastViewed * (2 - timeSinceLastViewed) / idealReviewTime;
-  };
-
-  let highestScore = 0; // do not select words with less then 0 score
-  let wordToReview: any | null = null;
-
-  for (const word of dictionary.items.value) {
-  	const importance = calculateImportance(word);
-  	const due = calculateScore(word);
-  	const random = 0.2 * Math.random();
-    const score = importance * due + random;
-    if (score > highestScore) {
-    	console.log(`Found new highest score for ${word.original}: ${score.toFixed(3)} (${importance.toFixed(2)}x${due.toFixed(2)} + ${random.toFixed(2)})`);
-      highestScore = score;
-      wordToReview = word;
-    }
-  }
-
-  return wordToReview?.id || null
-};
-
-const pickSentence = (sentences: { text: string, words: string[] }[]): number => {
-	const scores = sentences.map(sentence => {
-		const words = sentence.words.map(word => dictionary.find(word));
-		const score = words.reduce((acc, w) => acc + (w?.id === word.value?.id ? 6 : w?.reviewLevel || 0), 0);
-		const random = 1 + Math.random();
-		return random * score / words.length;
-	});
-
-	// find index with maximum score
-	let maxIndex = 0;
-	for (let i = 1; i < scores.length; i++) {
-		if (scores[i] > scores[maxIndex]) {
-			maxIndex = i;
-		}
-	}
-
-	return maxIndex;
-}
-
+const { dictionary, isLoading } = useDictionaryView(
+	word => !recentlyShown.includes(word.id)
+);
+const { pickSentence, findWordForReview } = useReview(dictionary);
 const wordId = ref<string | null>(null);
 const word = computed<WordDetail | null>(() => {
 	if (wordId.value) {		
