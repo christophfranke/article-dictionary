87ae7596f06e62414b17afab94fb9fa2a1a7b7d4
Christoph Franke <hallo@christophfranke.info>
2025-03-05 18:28:29 +0100

    update creation certificate

--- nginx/create-certificates.sh
+++ nginx/create-certificates.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 # Configuration variables
-DOMAINS="www.international-reader.com international-reader.com"
-EMAIL="public@krito.de"
+DOMAIN="international-reader.com"
+SUBDOMAINS="www.international-reader.com"
+EMAIL="public@christophfranke.info"
 WEBROOT_PATH="/var/www/certbot"
 
+# Prepare the -d options for Certbot
+DOMAINS_OPTION="-d $DOMAIN"
+for SUBDOMAIN in $SUBDOMAINS; do
+    DOMAINS_OPTION="$DOMAINS_OPTION -d $SUBDOMAIN"
+done
+
 # Run Certbot
-certbot certonly --webroot -w $WEBROOT_PATH -d $DOMAINS --email $EMAIL --agree-tos --no-eff-email
+certbot certonly --webroot -w $WEBROOT_PATH $DOMAINS_OPTION --email $EMAIL --agree-tos --no-eff-email
